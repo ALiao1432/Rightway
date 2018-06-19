ffe5d66d13fb62353b9489f44ea6a152badf2544
ALiao1432 <apple36578@gmail.com>
2018-06-19 15:30:40 +0800

    add select activity

--- app/src/main/java/com/study/ian/rightway/MainActivity.java
+++ app/src/main/java/com/study/ian/rightway/MainActivity.java
@@ -1,10 +1,12 @@
 package com.study.ian.rightway;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.study.ian.rightway.select.EventSelectActivity;
+import com.study.ian.rightway.select.SpeedSelectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSelectSpeed = findViewById(R.id.btnSelectSpeed);
+        Button btnSelectEvent = findViewById(R.id.btnSelectEvent);
+
+        Button.OnClickListener onClickListener = view -> {
+            Intent intent = new Intent();
+
+            switch (view.getId()) {
+                case R.id.btnSelectSpeed:
+                    intent.setClass(this, SpeedSelectActivity.class);
+                    break;
+                case R.id.btnSelectEvent:
+                    intent.setClass(this, EventSelectActivity.class);
+                    break;
+            }
+
+            startActivity(intent);
+        };
+
+        btnSelectSpeed.setOnClickListener(onClickListener);
+        btnSelectEvent.setOnClickListener(onClickListener);
     }
 }
---
+++ app/src/main/java/com/study/ian/rightway/select/EventSelectActivity.java
@@ -0,0 +1,15 @@
+package com.study.ian.rightway.select;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.study.ian.rightway.R;
+
+public class EventSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_select);
+    }
+}
