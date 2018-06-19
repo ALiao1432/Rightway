fbe7cb91a0177c869d98a4d0c72434466b42b731
ALiao1432 <apple36578@gmail.com>
2018-06-19 14:39:08 +0800

    add button in MainActivity

--- app/src/main/java/com/study/ian/rightway/MainActivity.java
+++ app/src/main/java/com/study/ian/rightway/MainActivity.java
@@ -1,7 +1,10 @@
 package com.study.ian.rightway;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setStatusBarColor(Color.parseColor("#212121"));
+    }
+
+    private void setStatusBarColor(int statusBarColor) {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(statusBarColor);
     }
 }
